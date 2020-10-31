e7bf2a4b47d47c48e72259c70d13a711772b2f75
Max Isom <hi@maxisom.me>
2020-10-31 10:42:06 -0400

    Automatically refresh Spotify token

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,18 @@ import {sequelize} from './utils/db';
 let bot = container.get<Bot>(TYPES.Bot);
 const spotify = container.get<Spotify>(TYPES.Lib.Spotify);
 
-(async () => {
+const refreshSpotifyToken = async () => {
   const auth = await spotify.clientCredentialsGrant();
 
   spotify.setAccessToken(auth.body.access_token);
+
+  return auth.body.expires_in;
+};
+
+(async () => {
+  const spotifyRefreshIntervalSeconds = await refreshSpotifyToken();
+
+  setInterval(async () => refreshSpotifyToken(), (spotifyRefreshIntervalSeconds / 2) * 1000);
 
   // Create data directories if necessary
   await makeDir(container.get(TYPES.Config.DATA_DIR));
