29c32ce65ec0c7103e4c895ec6e98207eb298ae6
Federico fuji97 Rapetti <fuji1097@gmail.com>
2021-12-27 16:53:36 +0100

    Refactor unskip command as slash command

--- src/commands/unskip.ts
+++ src/commands/unskip.ts
@@ -1,12 +1,17 @@
-import {Message} from 'discord.js';
+import {CommandInteraction, Message} from 'discord.js';
 import {TYPES} from '../types.js';
 import {inject, injectable} from 'inversify';
 import PlayerManager from '../managers/player.js';
 import errorMsg from '../utils/error-msg.js';
 import Command from '.';
+import {SlashCommandBuilder} from '@discordjs/builders';
 
 @injectable()
 export default class implements Command {
+  public readonly slashCommand = new SlashCommandBuilder()
+    .setName('unskip')
+    .setDescription('goes back in the queue by one song');
+
   public name = 'unskip';
   public aliases = ['back'];
   public examples = [
@@ -19,6 +24,21 @@ export default class implements Command {
 
   constructor(@inject(TYPES.Managers.Player) playerManager: PlayerManager) {
     this.playerManager = playerManager;
+  }
+
+  public async executeFromInteraction(interaction: CommandInteraction): Promise<void> {
+    const player = this.playerManager.get(interaction.guild!.id);
+
+    try {
+      await player.back();
+
+      await interaction.reply('back \'er up\'');
+    } catch (_: unknown) {
+      await interaction.reply({
+        content: errorMsg('no song to go back to'),
+        ephemeral: true,
+      });
+    }
   }
 
   public async execute(msg: Message, _: string []): Promise<void> {
