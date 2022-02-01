a25a967ab8f8a41d1ea484c119e682cb0373cdd6
Max Isom <hi@maxisom.me>
2022-02-01 18:32:32 -0500

    Fix Prisma path on Windows?
    See #475.

--- src/utils/create-database-url.ts
+++ src/utils/create-database-url.ts
@@ -2,6 +2,14 @@ import {join} from 'path';
 
 export const createDatabasePath = (directory: string) => join(directory, 'db.sqlite');
 
-const createDatabaseUrl = (directory: string) => `file:${createDatabasePath(directory)}`;
+const createDatabaseUrl = (directory: string) => {
+  const url = `file:${createDatabasePath(directory)}`;
+
+  if (process.platform === 'win32') {
+    return url.replaceAll(/\\/g, '\\\\');
+  }
+
+  return url;
+};
 
 export default createDatabaseUrl;
