a2f3c9d3922a0eaefe19e48aaf67896eb84798b7
Thongrapee Panyapatiphan <t.panyapatiphan@gmail.com>
2022-02-25 00:37:04 +0700

    feat: add stop command

---
+++ src/commands/stop.ts
@@ -0,0 +1,37 @@
+import {CommandInteraction} from 'discord.js';
+import {SlashCommandBuilder} from '@discordjs/builders';
+import {TYPES} from '../types.js';
+import {inject, injectable} from 'inversify';
+import PlayerManager from '../managers/player.js';
+import {STATUS} from '../services/player.js';
+import Command from '.';
+
+@injectable()
+export default class implements Command {
+  public readonly slashCommand = new SlashCommandBuilder()
+    .setName('stop')
+    .setDescription('stop playback, disconnect and clear all songs in the queue');
+
+  public requiresVC = true;
+
+  private readonly playerManager: PlayerManager;
+
+  constructor(@inject(TYPES.Managers.Player) playerManager: PlayerManager) {
+    this.playerManager = playerManager;
+  }
+
+  public async execute(interaction: CommandInteraction) {
+    const player = this.playerManager.get(interaction.guild!.id);
+
+    if (!player.voiceConnection) {
+      throw new Error('not connected');
+    }
+
+    if (player.status !== STATUS.PLAYING) {
+      throw new Error('not currently playing');
+    }
+
+    player.stop();
+    await interaction.reply('u betcha');
+  }
+}
