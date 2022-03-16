f65cbb3a37a3b13359e6ad03f3b8827f838927e1
Max Isom <codetheweb@users.noreply.github.com>
2022-03-15 21:25:52 -0400

    Add connection parameters to database URL (#575)

--- src/utils/create-database-url.ts
+++ src/utils/create-database-url.ts
@@ -3,7 +3,7 @@ import {join} from 'path';
 export const createDatabasePath = (directory: string) => join(directory, 'db.sqlite');
 
 const createDatabaseUrl = (directory: string) => {
-  const url = `file:${createDatabasePath(directory)}`;
+  const url = `file:${createDatabasePath(directory)}?socket_timeout=10&connection_limit=1`;
 
   if (process.platform === 'win32') {
     return url.replaceAll(/\\/g, '\\\\');
