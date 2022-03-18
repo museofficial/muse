2741189c3b2016b5b231a3988748e35cf6cbfbaa
Zagrthos <36556009+Zagrthos@users.noreply.github.com>
2022-03-18 23:49:22 +0100

    Next try

--- src/bot.ts
+++ src/bot.ts
@@ -18,7 +18,7 @@ import updatePermissionsForGuild from './utils/update-permissions-for-guild.js';
 
 @injectable()
 export default class {
-  private readonly client: Client;
+  private client: Client;
   private readonly token: string;
   private readonly shouldRegisterCommandsOnBot: boolean;
   private readonly commandsByName!: Collection<string, Command>;
@@ -122,13 +122,7 @@ export default class {
     this.client.once('ready', async () => {
       debug(generateDependencyReport());
 
-      this.client.user.setPresence({
-        status: 'online',
-        activity: {
-          name: 'with depression',
-          type: 'PLAYING',
-        },
-      });
+      this.client.user.setActivity('running with the wild things.');
 
       // Update commands
       const rest = new REST({version: '9'}).setToken(this.token);
