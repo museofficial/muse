31016d5ed748f0c6b80c964de7fcbf44292cd98a
Zagrthos <julian8150@hotmail.de>
2022-03-18 16:16:47 +0100

    Add Now Playing Command

---
+++ src/commands/now-playing.ts
@@ -0,0 +1,35 @@
+import {CommandInteraction} from 'discord.js';
+import {TYPES} from '../types.js';
+import {inject, injectable} from 'inversify';
+import PlayerManager from '../managers/player.js';
+import Command from '.';
+import {SlashCommandBuilder} from '@discordjs/builders';
+import {buildPlayingMessageEmbed} from '../utils/build-embed.js';
+
+@injectable()
+export default class implements Command {
+  public readonly slashCommand = new SlashCommandBuilder()
+    .setName('now-playing')
+    .setDescription('Zeigt an was gespielt wird.')
+
+  public requiresVC = true;
+
+  private readonly playerManager: PlayerManager;
+
+  constructor(@inject(TYPES.Managers.Player) playerManager: PlayerManager) {
+    this.playerManager = playerManager;
+  }
+
+  public async execute(interaction: CommandInteraction): Promise<void> {
+    const player = this.playerManager.get(interaction.guild!.id);
+
+    try {
+      await interaction.reply({
+        content: 'Das hier wird gerade gespielt!',
+        embeds: player.getCurrent() ? [buildPlayingMessageEmbed(player)] : [],
+      });
+    } catch (_: unknown) {
+      throw new Error('Du musst erst neues Zeug zum Spielen reinpacken!');
+    }
+  }
+}
