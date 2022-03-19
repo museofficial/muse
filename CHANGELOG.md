60376d4f5798004bc0215698bc1cc0786fcd5dbc
Zagrthos <36556009+Zagrthos@users.noreply.github.com>
2022-03-19 15:55:23 +0100

    Update README and add "Now playing" Command (#589)
    Co-authored-by: Max Isom <codetheweb@users.noreply.github.com>
    Co-authored-by: Max Isom <hi@maxisom.me>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Added a `/now-playing` command to show the current track without the full queue embed
 
 ## [1.6.2] - 2022-03-17
 ### Fixed
---
+++ src/commands/now-playing.ts
@@ -0,0 +1,32 @@
+import {CommandInteraction} from 'discord.js';
+import {TYPES} from '../types.js';
+import {inject, injectable} from 'inversify';
+import PlayerManager from '../managers/player.js';
+import Command from '.';
+import {SlashCommandBuilder} from '@discordjs/builders';
+import {buildPlayingMessageEmbed} from '../utils/build-embed.js';
+
+@injectable()
+export default class implements Command {
+  public readonly slashCommand = new SlashCommandBuilder()
+    .setName('now-playing')
+    .setDescription('shows the currently played song');
+
+  private readonly playerManager: PlayerManager;
+
+  constructor(@inject(TYPES.Managers.Player) playerManager: PlayerManager) {
+    this.playerManager = playerManager;
+  }
+
+  public async execute(interaction: CommandInteraction): Promise<void> {
+    const player = this.playerManager.get(interaction.guild!.id);
+
+    if (!player.getCurrent()) {
+      throw new Error('nothing is currently playing');
+    }
+
+    await interaction.reply({
+      embeds: [buildPlayingMessageEmbed(player)],
+    });
+  }
+}
