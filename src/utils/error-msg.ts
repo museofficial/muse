eac9ec48b12bacdb6a69415f6cd75455ea49b873
Jefferey Neuffer <35882868+j7126@users.noreply.github.com>
2023-01-25 13:13:32 +1000

    Minor message improvements (#865)

--- src/utils/error-msg.ts
+++ src/utils/error-msg.ts
@@ -3,9 +3,9 @@ export default (error?: string | Error): string => {
 
   if (error) {
     if (typeof error === 'string') {
-      str = `🚫ope: ${error}`;
+      str = `🚫 ope: ${error}`;
     } else if (error instanceof Error) {
-      str = `🚫ope: ${error.message}`;
+      str = `🚫 ope: ${error.message}`;
     }
   }
 
