815a767a8652ed03bc7e20dc8556d36804461631
xHyperElectric <laskew21@yahoo.com>
2023-02-26 03:26:16 -0500

    add /replay command

---
+++ src/commands/replay.ts
@@ -0,0 +1,42 @@
+import {ChatInputCommandInteraction} from 'discord.js';
+import {TYPES} from '../types.js';
+import {inject, injectable} from 'inversify';
+import PlayerManager from '../managers/player.js';
+import Command from '.';
+import {SlashCommandBuilder} from '@discordjs/builders';
+
+@injectable()
+export default class implements Command {
+  public readonly slashCommand = new SlashCommandBuilder()
+    .setName('replay')
+    .setDescription('replay the current song');
+
+  public requiresVC = true;
+
+  private readonly playerManager: PlayerManager;
+
+  constructor(@inject(TYPES.Managers.Player) playerManager: PlayerManager) {
+    this.playerManager = playerManager;
+  }
+
+  public async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const player = this.playerManager.get(interaction.guild!.id);
+
+    const currentSong = player.getCurrent();
+
+    if (!currentSong) {
+      throw new Error('nothing is playing');
+    }
+
+    if (currentSong.isLive) {
+      throw new Error('can\'t replay a livestream');
+    }
+
+    await Promise.all([
+      player.seek(0),
+      interaction.deferReply(),
+    ]);
+
+    await interaction.editReply('👍 replayed the current song');
+  }
+}
