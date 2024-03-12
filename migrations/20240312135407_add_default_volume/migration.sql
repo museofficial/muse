52d2ccba1f2bd02253e29a305b72cf95681d5b65
FoxxMD <FoxxMD@users.noreply.github.com>
2024-03-12 10:05:54 -0400

    fix: Regenerate volume migration
    ccd8793cc1d8c91423cd37926b51ca283e3a8c39 introduced a new migration so we need to regenerate since the new migration does not include defaultVolume

--- migrations/20240312135407_add_default_volume/migration.sql
+++ migrations/20240312135407_add_default_volume/migration.sql
@@ -5,11 +5,12 @@ CREATE TABLE "new_Setting" (
     "playlistLimit" INTEGER NOT NULL DEFAULT 50,
     "secondsToWaitAfterQueueEmpties" INTEGER NOT NULL DEFAULT 30,
     "leaveIfNoListeners" BOOLEAN NOT NULL DEFAULT true,
+    "autoAnnounceNextSong" BOOLEAN NOT NULL DEFAULT false,
     "defaultVolume" INTEGER NOT NULL DEFAULT 100,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
-INSERT INTO "new_Setting" ("createdAt", "guildId", "leaveIfNoListeners", "playlistLimit", "secondsToWaitAfterQueueEmpties", "updatedAt") SELECT "createdAt", "guildId", "leaveIfNoListeners", "playlistLimit", "secondsToWaitAfterQueueEmpties", "updatedAt" FROM "Setting";
+INSERT INTO "new_Setting" ("autoAnnounceNextSong", "createdAt", "guildId", "leaveIfNoListeners", "playlistLimit", "secondsToWaitAfterQueueEmpties", "updatedAt") SELECT "autoAnnounceNextSong", "createdAt", "guildId", "leaveIfNoListeners", "playlistLimit", "secondsToWaitAfterQueueEmpties", "updatedAt" FROM "Setting";
 DROP TABLE "Setting";
 ALTER TABLE "new_Setting" RENAME TO "Setting";
 PRAGMA foreign_key_check;
